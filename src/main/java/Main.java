ec3d307a94dfa6e904057fd80ef20732554fb9a8
jbt21050 <jbt21050@yahoo.co.jp>
2025-03-03 11:55:57 +0000

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,8 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    var radius = 5;
+    System.out.println(radius * radius * 3.14);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    System.out.println(((Object)radius).getClass().getSimpleName());
+  }
 }
