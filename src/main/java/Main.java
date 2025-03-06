269101359c259812bae4575d8c87b1176b5011c4
jbt21050 <jbt21050@yahoo.co.jp>
2025-03-06 07:18:24 +0000

    繰り返しfor文２
    繰り返し

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-public class Main {
-  public static void main(String[] args) {
-   int[] scores = {1,5,10};
+import java.util.ArrayList;
 
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> scores = new ArrayList<Integer>();
+    
+    scores.add(1);
+    scores.add(5);
+    scores.add(10);
+    scores.add(15);
+  
     for(int score : scores) {
-      for (int i = 0; i < 3; i++)
       System.out.println(score);
     }
   }
