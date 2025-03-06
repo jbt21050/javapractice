f2e43a37fa24f8d58a600cbc5293178321113f05
jbt21050 <jbt21050@yahoo.co.jp>
2025-03-06 06:33:49 +0000

    7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-   int value = 3;
+   int value = 5;
 
    if (value > 0){
      System.out.println("値は正です");
+   }else if (value < 0){
+     System.out.println("値は負です");
+   }else {
+     System.out.println("値は０です");
    }
   }
 }
