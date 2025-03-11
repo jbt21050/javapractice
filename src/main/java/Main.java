11fc94f8567309670ce0df0b1efc26eb10cd0d17
jbt21050 <jbt21050@yahoo.co.jp>
2025-03-11 14:29:15 +0000

    メソット１

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,10 @@
-import java.util.ArrayList;
-
 class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> scores = new ArrayList<Integer>();
-    
-    scores.add(1);
-    scores.add(5);
-    scores.add(10);
-    scores.add(15);
-  
-    for(int score : scores) {
-      System.out.println(score);
-    }
+   sayHello();
+  }
+
+  public static void sayHello() {
+    System.out.println("Hello World");
+    return;
   }
 }
